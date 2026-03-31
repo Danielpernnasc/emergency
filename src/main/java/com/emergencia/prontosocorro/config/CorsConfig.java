6dd5846033a892703f088f077b54b7e3993fa493
Daniel Nascimento <nascimento@NascimentoDan.localdomain>
2026-03-31 10:55:44 -0300

    atualizando

--- src/main/java/com/emergencia/prontosocorro/config/CorsConfig.java
+++ src/main/java/com/emergencia/prontosocorro/config/CorsConfig.java
@@ -2,11 +2,13 @@ package com.emergencia.prontosocorro.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.cors.*;
 import org.springframework.web.filter.CorsFilter;
 
 import java.util.List;
 
+@Profile("!test")
 @Configuration
 public class CorsConfig {
 
@@ -17,7 +19,7 @@ public class CorsConfig {
         config.setAllowCredentials(true);
         config.setAllowedOriginPatterns(List.of("*")); // aceita tudo (debug)
         config.setAllowedHeaders(List.of("*"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
