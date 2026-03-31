50b92b3a475490b6cc598b2a36b8ce6ce77eb8bd
Daniel Nascimento <nascimento@NascimentoDan.localdomain>
2026-03-31 11:12:27 -0300

    atualiza

--- src/test/java/com/emergencia/prontosocorro/ProntosocorroApplicationTest.java
+++ src/test/java/com/emergencia/prontosocorro/ProntosocorroApplicationTest.java
@@ -1,18 +1,23 @@
 package com.emergencia.prontosocorro;
 
 
-
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import org.springframework.test.context.ActiveProfiles;
 
 
+
+
+
+@ActiveProfiles("test")
 class ProntosocorroApplicationTest {
 
 
     @Test
     void shouldLoadContext() {
-     
+         assertTrue(true);
     }
 
   }
