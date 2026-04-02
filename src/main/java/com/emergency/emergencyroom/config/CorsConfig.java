0177d047cd32cf23052cede94cd9f9cbd2b7ddad
Daniel Nascimento <nascimento@NascimentoDan.localdomain>
2026-04-02 15:28:29 -0300

    test

--- src/main/java/com/emergency/emergencyroom/config/CorsConfig.java
+++ src/main/java/com/emergency/emergencyroom/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.emergency.emergencyroom.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -11,6 +12,11 @@ import java.util.List;
 @Profile("!test")
 @Configuration
 public class CorsConfig {
+    @Value("${app.url.prod}")
+    private String prodUrl;
+
+    @Value("${app.url.local}")
+    private String localUrl;
 
     @Bean
     public CorsFilter corsFilter() {
@@ -20,6 +26,7 @@ public class CorsConfig {
         config.setAllowedOriginPatterns(List.of("*")); // aceita tudo (debug)
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        config.setAllowedOriginPatterns(List.of(prodUrl, localUrl));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
